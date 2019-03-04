7e7c415d02eaa30bca5cf21203dea239e7fc3e1f
unknown <1723109431@qq.com>
2019-03-04 10:14:08 +0800

    spring study

---
+++ quickstart/src/main/java/com/spring/quickstart/quickstart/hello/StudentApp.java
@@ -0,0 +1,14 @@
+package com.spring.quickstart.quickstart.hello;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class StudentApp {
+    public static void main(String[] args) {
+        //读入XML文件
+        ApplicationContext context = new ClassPathXmlApplicationContext("/beans.xml");
+        //读取配置好的beans
+        Student student= (Student) context.getBean("student");
+        System.out.print(student);
+    }
+}
