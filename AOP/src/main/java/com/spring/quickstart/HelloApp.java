1d839dbe0bc9a68448b1e17f6d134959d13babd0
ASUS <1723109431@qq.com>
2019-03-09 20:02:40 +0800

    spring study 4

--- AOP/src/main/java/com/spring/quickstart/HelloApp.java
+++ AOP/src/main/java/com/spring/quickstart/HelloApp.java
@@ -7,7 +7,7 @@ public class HelloApp {
     public static void main(String[] args) {
         ApplicationContext context=new ClassPathXmlApplicationContext("/applicationContext.xml");
         Hello hello=context.getBean(Hello.class);
-        System.out.println(hello);
+        System.out.println(hello.getHello());
     }
 
 
